89c623128f3963e17b043e50ab34f8a84cf6f101
Yashwanth Racherla <ignitingyash@gmail.com>
2022-07-03 21:33:21 +0530

    Search Section and Movies Section CSS WIP

--- my-movies/src/pages/Home.jsx
+++ my-movies/src/pages/Home.jsx
@@ -5,19 +5,33 @@ const Home = () => {
   const { movies } = useGlobalContext();
 
   return (
-    <div className="container grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-      {movies.map((movie) => {
-        const { Title, Poster, Type, Year, imdbID } = movie;
-        return (
-          <div
-            className="border-2 border-black rounded-xl flex flex-col items-center justify-center"
-            key={imdbID}
-          >
-            <h3 className="font-bold text-xl mb-2 text-center">{Title}</h3>
-            <img className="w-4/5 h-4/5" src={Poster} alt={imdbID} />
-          </div>
-        );
-      })}
+    <div className="bg-[#f2f4fc] text-[#4a5c6c]">
+      <div className="section-search container">
+        <p>Search Your Favourite Movie</p>
+        <label htmlFor="movie"></label>
+        <input
+          type="text"
+          value="Doctor Strange"
+          id="movie"
+          className="px-4 py-1 border-2 border-[#9cb0c1] "
+        />
+      </div>
+
+      <div className="section-movies container ">
+        {movies.map((movie) => {
+          const { Title, Poster, Type, Year, imdbID } = movie;
+          return (
+            <div className="border-2 border-black rounded-xl p-8 " key={imdbID}>
+              <h3 className="font-bold text-xl mb-2 text-center">{Title}</h3>
+              <img
+                className="w-4/5 h-4/5 rounded-xl"
+                src={Poster}
+                alt={imdbID}
+              />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
